47f08e57293f18a0231edfa5cccecf132e261fc7
Brandon Foltz <bsfoltzguitar@gmail.com>
2013-02-25 16:45:22 -0500

    Rewrote printInt function to be less memory consuming.

--- sharedlib/print.c
+++ sharedlib/print.c
@@ -13,29 +13,47 @@ printv(const char *string, void *var, enum e_VarType type)
 void 
 printInt(long input, enum e_VarType type)
 {
-	//works well for unsigned ints...
-	//SURELY there is a better way that requires less ram
-	char myString[8] = "";
 	char myDigits[8] = "";
 	
-	if (type == VAR_UNSIGNED)
+	if (type == VAR_UNSIGNED) //maybe dif function for signed/unsigned?
 	{
 		unsigned char numDigits = 0;
 		do {
-
-		//NOTE bad habit of manipulating input variable (parameter)...
-		unsigned char digit = input % 10;
-		myDigits[numDigits] = '0' + digit;
-		numDigits++;
-		input /= 10;
-
+			//NOTE bad habit of manipulating input variable (parameter)...
+			unsigned char digit = input % 10;
+			myDigits[numDigits] = '0' + digit;
+			numDigits++;
+			input /= 10;
 		} while (input > 0);
 
-		for (unsigned char i = 0; i < numDigits; i++)
-			myString[i] = myDigits[numDigits - 1 - i];
+		char tempDigit = 0;
+		if (numDigits % 2 == 0) //if number of digits is even, we need to change algorithm
+		{
+			char i;
+			for (i = 0; i < numDigits; i++)
+			{
+				tempDigit = myDigits[i];
+				myDigits[i] = myDigits[numDigits - 1 - i];
+				myDigits[numDigits - 1 - i] = tempDigit;
+				if (i + 1 == numDigits - 1 - i)
+					break;
+			}
+		}
+		else
+		{
+			char i;
+			for (i = 0; i < numDigits; i++)
+			{
+				tempDigit = myDigits[i];
+				myDigits[i] = myDigits[numDigits - 1 - i];
+				myDigits[numDigits - 1 - i] = tempDigit;
+				if (i == numDigits - 1 - i)
+					break;
+			}
+		}
 
-		myString[numDigits] = '\0';
-		CON_SendRAMString(myString);
+		myDigits[numDigits] = '\0';
+		CON_SendRAMString(myDigits);
 	}
 }
 

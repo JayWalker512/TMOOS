87175ea8a6e6264883a88b7250f28407a0358178
Brandon Foltz <bsfoltzguitar@gmail.com>
2013-07-28 18:38:40 -0400

    Progress on Pong. Paddles, paddle collision.

--- games/pong.c
+++ games/pong.c
@@ -1,6 +1,7 @@
 #include "gamelib.h"
+#include "../display/display.h"
 
-#define PADDLE_WIDTH 4
+#define PADDLE_WIDTH 3
 
 typedef struct s_PongPlayer {
 	char x;
@@ -22,15 +23,15 @@ unsigned long lastTime;
 void RenderPong(void);
 void CheckForCollisions(void);
 void UpdateBall(unsigned long dt);
-void UpdatePaddles(unsigned long dt);
+void UpdatePlayerPaddle(unsigned long dt);
 
 char 
 InitPongGame(void)
 {
 	pongBall.x = 7;
 	pongBall.y = 7;
-	pongBall.xSpeed = 0;
-	pongBall.ySpeed = 0;
+	pongBall.xSpeed = 0.010;
+	pongBall.ySpeed = 0.007;
 	
 	player.x = 6;
 	player.score = 0;
@@ -49,11 +50,97 @@ PongGameLoop(void)
 	dt = thisTime - lastTime;
 	lastTime = thisTime;
 	
+	UpdateBall(dt);
+	UpdatePlayerPaddle(dt);
+	CheckForCollisions();
 	RenderPong();
 }
 
 void 
 RenderPong(void)
 {
+	GFX_Clear(0);
 	
+	//draw ball
+	GFX_PutPixel(floor(pongBall.x), floor(pongBall.y), 1);
+	
+	//draw player paddle
+	GFX_DrawLine(player.x, DISPLAY_HEIGHT - 1, 
+		player.x + PADDLE_WIDTH, DISPLAY_HEIGHT - 1);
+	
+	//draw comp paddle
+	GFX_DrawLine(comp.x, 0, comp.x + PADDLE_WIDTH, 0);
+	
+	GFX_SwapBuffers();
+}
+
+void CheckForCollisions(void)
+{
+	//bounce off walls
+	if (pongBall.x >= DISPLAY_WIDTH)
+	{
+		pongBall.x = DISPLAY_WIDTH;
+		pongBall.xSpeed = -pongBall.xSpeed;
+	}
+	
+	if (pongBall.x <= 0)
+	{
+		pongBall.x = 0;
+		pongBall.xSpeed = -pongBall.xSpeed;
+	}
+	
+	if (pongBall.y >= DISPLAY_HEIGHT)
+	{
+		pongBall.y = DISPLAY_HEIGHT;
+		pongBall.ySpeed = -pongBall.ySpeed; 
+	}
+	
+	if (pongBall.y <= 0)
+	{
+		pongBall.y = 0;
+		pongBall.ySpeed = -pongBall.ySpeed;
+	}
+	
+	//bounce ball off paddles
+	if (pongBall.y >= 15 && pongBall.y <= DISPLAY_HEIGHT)
+	{
+		if (pongBall.x >= player.x && 
+			pongBall.x <= player.x + PADDLE_WIDTH)
+		{
+			pongBall.ySpeed = -pongBall.ySpeed;
+			pongBall.y = 15;
+		}
+		else if (pongBall.x >= comp.x && 
+			pongBall.x <= comp.x + PADDLE_WIDTH)
+		{
+			pongBall.ySpeed = -pongBall.ySpeed;
+			pongBall.y = 15;
+		}	
+	}
+	else if (pongBall.y <= 1 && pongBall.y >= 0)
+	{
+		if (pongBall.x >= player.x && 
+			pongBall.x <= player.x + PADDLE_WIDTH)
+		{
+			pongBall.ySpeed = -pongBall.ySpeed;
+			pongBall.y = 1;
+		}
+		else if (pongBall.x >= comp.x && 
+			pongBall.x <= comp.x + PADDLE_WIDTH)
+		{
+			pongBall.ySpeed = -pongBall.ySpeed;
+			pongBall.y = 1;
+		}	
+	}
+}
+
+void UpdateBall(unsigned long dt)
+{
+	pongBall.x += pongBall.xSpeed * dt;
+	pongBall.y += pongBall.ySpeed * dt;
+}
+
+void UpdatePlayerPaddle(unsigned long dt)
+{
+	player.x = GLIB_GetWheelRegion(DISPLAY_HEIGHT - PADDLE_WIDTH);
 }

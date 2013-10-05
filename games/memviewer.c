0a5433d808513c880f5cbbb9cfa7b1a0c5350da4
Brandon Foltz <bsfoltzguitar@gmail.com>
2013-10-04 23:06:49 -0400

    Fixed warnings in memviewer.c

--- games/memviewer.c
+++ games/memviewer.c
@@ -7,6 +7,8 @@ char MemViewer(void)
 	unsigned char region = GLIB_GetWheelRegion(TEENSY_MEMORY / 32);
 	
 	GFX_Clear(0);
-	GFX_BitBLT((region * 32), 16,16,0,0);
+	GFX_BitBLT((const char * const)(region * 32), 16,16,0,0);
 	GFX_SwapBuffers();
+	
+	return 1;
 }
